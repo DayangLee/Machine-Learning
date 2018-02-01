6ce4210d24eea63863c8dc8f4a2104546242bd15
LJY <dayanglee2015@163.com>
2018-02-01 23:40:39 +0800

    update demo1

--- demo1/recommendations.py
+++ demo1/recommendations.py
@@ -122,3 +122,36 @@ def topMatches(prefs, person, n=5, similarity=sim_pearson):
     scores.sort()
     scores.reverse()
     return scores[0:n]
+
+
+# 利用所有他人评价值的加权平均，为某人提供建议
+def getRecommendations(prefs,person,similarity=sim_pearson):
+    totals={}
+    simSums={}
+    for other in prefs:
+        # 不要和自己作比较
+        if other == person:
+            continue
+        sim=similarity(prefs,person,other)
+
+        # 忽略评价值为0或小于0的情况
+        if sim<=0:
+            continue
+        for item in prefs[other]:
+
+            # 只对自己还未曾看过的影片进行评价
+            if item not in prefs[person] or prefs[person][item] ==0:
+                # 相似度*评价值
+                totals.setdefault(item,0)
+                totals[item]+=prefs[other][item]*sim
+                # 相似度之和
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
+
+    # 建立一个归一化的列表
+    rankings=[(total/simSums[item],item) for item,total in totals.items()]
+
+    # 返回经过排序的列表
+    rankings.sort()
+    rankings.reverse()
+    return rankings
