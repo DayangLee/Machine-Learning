ce6a4ae2c84ef85dc84a0f0712cb339c435829df
LJY <dayanglee2015@163.com>
2018-02-06 18:22:05 +0800

    update recommendations.py of demo1

--- demo1/recommendations.py
+++ demo1/recommendations.py
@@ -125,33 +125,63 @@ def topMatches(prefs, person, n=5, similarity=sim_pearson):
 
 
 # 利用所有他人评价值的加权平均，为某人提供建议
-def getRecommendations(prefs,person,similarity=sim_pearson):
-    totals={}
-    simSums={}
+def getRecommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    simSums = {}
     for other in prefs:
         # 不要和自己作比较
         if other == person:
             continue
-        sim=similarity(prefs,person,other)
+        sim = similarity(prefs, person, other)
 
         # 忽略评价值为0或小于0的情况
-        if sim<=0:
+        if sim <= 0:
             continue
         for item in prefs[other]:
 
             # 只对自己还未曾看过的影片进行评价
-            if item not in prefs[person] or prefs[person][item] ==0:
+            if item not in prefs[person] or prefs[person][item] == 0:
                 # 相似度*评价值
-                totals.setdefault(item,0)
-                totals[item]+=prefs[other][item]*sim
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
                 # 相似度之和
-                simSums.setdefault(item,0)
-                simSums[item]+=sim
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
 
     # 建立一个归一化的列表
-    rankings=[(total/simSums[item],item) for item,total in totals.items()]
+    rankings = [(total / simSums[item], item)
+                for item, total in totals.items()]
 
     # 返回经过排序的列表
     rankings.sort()
     rankings.reverse()
     return rankings
+
+
+def transformPrefs(prefs):
+    result = {}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item, {})
+
+            # 将物品和人员对调
+            result[item][person] = prefs[person][item]
+    return result
+
+
+def calculateSimilarItems(prefs, n=10):
+    # 建立字典，以给出与这些物品最为相近的所有其他物品
+    result = {}
+
+    # 以物品为中心对偏好矩阵实施倒置处理
+    itemPrefs = transformPrefs(prefs)
+    c = 0
+    for item in itemPrefs:
+        # 针对大数据集更新状态变量
+        c += 1
+        if c % 100 == 0:
+            print("%d / %d" % (c, len(itemPrefs)))
+        # 寻找最为相近的物品
+        scores = topMatches(itemPrefs, item, n=n, similarity=sim_distance)
+        result[item] = scores
+    return result
