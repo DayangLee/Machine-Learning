1e80fa28fceb114e637b87bdc47d884cfa4f2d42
LJY <dayanglee2015@163.com>
2018-03-09 19:16:19 +0800

    update exa of Coursera

--- Coursera/ex1/ex1/gradientDescent.m
+++ Coursera/ex1/ex1/gradientDescent.m
@@ -16,10 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-    delta = (1/m)*sum(X.*repmat((X*theta - y), 1, size(X,2)));
-       
-    theta = (theta' - (alpha * delta))';
+    
+    label = theta;
+    for i=1:length(theta)
+       label(i) = theta(i) -alpha/m*(X*theta-y)'*X(:,i); 
+    end
+    theta = label;
 
     % ============================================================
 
